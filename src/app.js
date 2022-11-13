f294e48ca2b28f96f44aa5c133f17857e5196ccf
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2022-11-12 19:25:13 -0500

    created repo, created folders, installs, json files

---
+++ src/app.js
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+    return (
+        <div>Hello World</div>
+    )
+}
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './app';
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+// export default App;
