48c33f9aaef936555b12f12bb18e8c61a652cdc7
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2022-11-12 19:59:26 -0500

    added to the json package, installs, created app function

--- src/app.js
+++ src/app.js
@@ -1,17 +1,11 @@
+import React from 'react';
 
 
-
-function App() {
+export default function App() {
     return (
         <div>Hello World</div>
     )
 }
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './app';
 
 
-ReactDOM.render(<App />, document.getElementById('root'))
-
-// export default App;
